99cc2adc85cda3ca6f7ee31fba7105ecd73ce414
Eldissone <eldissone@gmail.com>
2026-05-05 14:10:39 +0100

    update conf

--- frontend/server.js
+++ frontend/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 10000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle SPA routing: redirect all requests to index.html
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
